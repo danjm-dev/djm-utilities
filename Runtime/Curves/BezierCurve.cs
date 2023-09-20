8afdd05ce456bf84fd22ff532ca118a500f55330
Daniel Miller <danjm.dev@gmail.com>
2023-09-20 17:25:36 +1200

    Added 2 alternative cubic bezier implementations

--- Runtime/Curves/BezierCurve.cs
+++ Runtime/Curves/BezierCurve.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace DJM.Utilities.Curves
 {
-    public static class BezierCurve
+    public static class BezierCurve // https://www.youtube.com/watch?v=jvPPXbo87ds
     {
         public static Vector3 Quadratic(Vector3 point0, Vector3 point1, Vector3 point2,  float t)
         {
@@ -11,6 +11,7 @@ namespace DJM.Utilities.Curves
             return Vector3.Lerp(q0, q1, t);
         }
         
+        // DeCasteljau
         public static Vector3 Cubic(Vector3 point0, Vector3 point1, Vector3 point2, Vector3 point3,  float t)
         {
             var q0 = Vector3.Lerp(point0, point1, t);
@@ -23,6 +24,30 @@ namespace DJM.Utilities.Curves
             return Vector3.Lerp(r0, r1, t);
         }
         
+        // Bernstein
+        public static Vector3 Cubic2(Vector3 point0, Vector3 point1, Vector3 point2, Vector3 point3,  float t)
+        {
+            var tPow2 = Mathf.Pow(t, 2);
+            var tPow3 = Mathf.Pow(t, 3);
+            
+            var p0 = point0 * (-tPow3 + 3 * tPow2 - 3 * t + 1);
+            var p1 = point1 * (3 * tPow3 - 6 * tPow2 + 3 * t);
+            var p2 = point2 * (-3 * tPow3 + 3 * tPow2); 
+            var p3 = point3 * tPow3;
+
+            return p0 + p1 + p2 + p3;
+        }
+        
+        // polynomial coefficients
+        public static Vector3 Cubic3(Vector3 point0, Vector3 point1, Vector3 point2, Vector3 point3,  float t)
+        {
+            var a = t * (-3 * point0 + 3 * point1);
+            var b = Mathf.Pow(t, 2) * (3 * point0 - 6 * point1 + 3 * point2);
+            var c = Mathf.Pow(t, 3) * (-point0 + 3 * point1 - 3 * point2 + point3);
+            return point0 + a + b + c;
+        }
+        
+        // Bernstein derivative
         public static Vector3 CubicDerivative(Vector3 point0, Vector3 point1, Vector3 point2, Vector3 point3,  float t)
         {
             var tPower2 = Mathf.Pow(t, 2);
