afab29bd99edbff7adcee79c2dd34027ad9ac867
Daniel Miller <danjm.dev@gmail.com>
2023-10-11 20:17:38 +1300

    Updated quad mesh class name. exposed its uvs and tris as static readonly members

--- Runtime/Curves/DJMBezierCurve.cs
+++ Runtime/Curves/DJMBezierCurve.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-namespace DJM.Utilities
+namespace DJM.Utilities.Curves
 {
     public static class DJMBezierCurve // https://www.youtube.com/watch?v=jvPPXbo87ds
     {
--- Runtime/MeshGeneration/DJMQuadMesh.cs
+++ Runtime/MeshGeneration/DJMQuadMesh.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
-namespace DJM.Utilities
+namespace DJM.Utilities.MeshGeneration
 {
-    public static class DJMMesh
+    public static class DJMQuadMesh
     {
-        public static Mesh Quad(Vector3 centerPosition, float width, float height, Vector3 normal)
+        public static readonly ushort[] Triangles = { 0, 1, 2, 0, 2, 3 };
+        public static readonly Vector2[] Uvs = { new(0, 0), new(0, 1), new(1, 1), new(1, 0) };
+        
+        public static Mesh Create(Vector3 centerPosition, float width, float height, Vector3 normal)
         {
-            // Check if normal is not mostly pointing up/down
+            // Check if normal is roughly pointing up/down
             var notParallel = Mathf.Abs(Vector3.Dot(normal, Vector3.up)) < 0.99f ? Vector3.up : Vector3.right; 
-
-            // Use the notParallel vector to get the halfWidthOffset 
+            
             var halfWidthOffset = Vector3.Normalize(Vector3.Cross(normal, notParallel)) * width * 0.5f;
             var halfHeightOffset = Vector3.Normalize(Vector3.Cross(halfWidthOffset, normal)) * height * 0.5f;
             
@@ -22,16 +24,14 @@ namespace DJM.Utilities
                 centerPosition + halfWidthOffset - halfHeightOffset
             };
             
-            var tris = new ushort[] { 0, 1, 2, 0, 2, 3 };
             var normals = new [] { normal, normal, normal, normal };
-            var uvs = new Vector2[] { new (0, 0), new (0, 1), new (1, 1), new (1, 0) };
             
             var mesh = new Mesh();
             mesh.indexFormat = IndexFormat.UInt16;
             mesh.vertices = vertices;
-            mesh.uv = uvs;
+            mesh.uv = Uvs;
             mesh.normals = normals;
-            mesh.SetTriangles(tris, 0);
+            mesh.SetTriangles(Triangles, 0);
             return mesh;
         } 
     }
