a8ea6aff06dcfaf02173bfb9535ccfc3dbf3bcf9
Dan <danjm.dev@gmail.com>
2024-08-27 13:55:30 +1200

    Fixed error in DebugUtils

--- Runtime/Debugging/DebugUtils.cs
+++ Runtime/Debugging/DebugUtils.cs
@@ -6,19 +6,19 @@ namespace DJM.Utilities.Debugging
 {
     public static class DebugUtils
     {
-        [Conditional("UNITY_EDITOR || DEVELOPMENT_BUILD")]
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         public static void Log(object message, Object context = null)
         {
             Debug.Log(message, context);
         }
         
-        [Conditional("UNITY_EDITOR || DEVELOPMENT_BUILD")]
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         public static void LogWarning(object message, Object context = null)
         {
             Debug.LogWarning(message, context);
         }
                 
-        [Conditional("UNITY_EDITOR || DEVELOPMENT_BUILD")]
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         public static void LogError(object message, Object context = null)
         {
             Debug.LogError(message, context);
