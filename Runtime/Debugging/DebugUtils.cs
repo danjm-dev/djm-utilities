6c5ff2275db49ea34ae0d1a86b2382e5dbbc3901
Dan <danjm.dev@gmail.com>
2024-08-27 13:53:33 +1200

    Added DebugUtils

---
+++ Runtime/Debugging/DebugUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+
+namespace DJM.Utilities.Debugging
+{
+    public static class DebugUtils
+    {
+        [Conditional("UNITY_EDITOR || DEVELOPMENT_BUILD")]
+        public static void Log(object message, Object context = null)
+        {
+            Debug.Log(message, context);
+        }
+        
+        [Conditional("UNITY_EDITOR || DEVELOPMENT_BUILD")]
+        public static void LogWarning(object message, Object context = null)
+        {
+            Debug.LogWarning(message, context);
+        }
+                
+        [Conditional("UNITY_EDITOR || DEVELOPMENT_BUILD")]
+        public static void LogError(object message, Object context = null)
+        {
+            Debug.LogError(message, context);
+        }
+    }
+}
