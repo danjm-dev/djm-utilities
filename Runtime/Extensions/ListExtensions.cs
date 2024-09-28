b41fafdda6011e77bce97352856474d08294b620
Dan <danjm.dev@gmail.com>
2024-09-28 16:31:35 +1200

    Updated list extensions

--- Runtime/Extensions/ListExtensions.cs
+++ Runtime/Extensions/ListExtensions.cs
@@ -13,12 +13,22 @@ namespace DJM.Utilities
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsValidIndex<T>(this T[] array, int index) => index >= 0 && index < array.Length;
         
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsFirstIndex<T>(this IList<T> list, int index) => index == 0;
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsFirstIndex<T>(this IReadOnlyList<T> list, int index) => index == 0;
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsFirstIndex<T>(this T[] array, int index) => index == 0;
+        
+        
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsLastIndex<T>(this IList<T> list, int index) => index == list.Count - 1;
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsLastIndex<T>(this IReadOnlyList<T> list, int index) => index == list.Count - 1;
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsLastIndex<T>(this T[] array, int index) => index == array.Length - 1;
+        
         
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsFirstOrLastIndex<T>(this IList<T> list, int index) => index == 0 || index == list.Count - 1;
