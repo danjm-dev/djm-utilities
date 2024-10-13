6bcf50a18cd4da5cb17a6463320930c0667962ec
Dan <danjm.dev@gmail.com>
2024-10-14 12:37:32 +1300

    added implicit cast from float to duration

--- Runtime/Time/Duration.cs
+++ Runtime/Time/Duration.cs
@@ -5,15 +5,16 @@ namespace DJM.Utilities.Time
     public readonly struct Duration
     {
         public readonly float BaseDuration;
-        public readonly float RandomOffset;
-        public float Value => BaseDuration + Random.Range(-RandomOffset, RandomOffset);
+        public readonly float MaxRandomOffset;
+        public float Value => BaseDuration + Random.Range(-MaxRandomOffset, MaxRandomOffset);
         
-        public Duration(float baseDuration, float randomOffset = 0f)
+        public Duration(float baseDuration, float maxRandomOffset = 0f)
         {
             BaseDuration = Mathf.Max(0f, baseDuration);
-            RandomOffset = Mathf.Max(0f, randomOffset);
+            MaxRandomOffset = Mathf.Max(0f, maxRandomOffset);
         }
         
-        public static implicit operator float(Duration duration) => duration.Value; 
+        public static implicit operator float(Duration duration) => duration.Value;
+        public static implicit operator Duration(float duration) => new(duration);
     }
 }
