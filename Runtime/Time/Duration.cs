f96ee9c311add4e9296c1cb22c35e189b3ef1274
Dan <danjm.dev@gmail.com>
2024-10-14 12:25:44 +1300

    Added duration struct

---
+++ Runtime/Time/Duration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DJM.Utilities.Time
+{
+    public readonly struct Duration
+    {
+        public readonly float BaseDuration;
+        public readonly float RandomOffset;
+        public float Value => BaseDuration + Random.Range(-RandomOffset, RandomOffset);
+        
+        public Duration(float baseDuration, float randomOffset = 0f)
+        {
+            BaseDuration = Mathf.Max(0f, baseDuration);
+            RandomOffset = Mathf.Max(0f, randomOffset);
+        }
+        
+        public static implicit operator float(Duration duration) => duration.Value; 
+    }
+}
