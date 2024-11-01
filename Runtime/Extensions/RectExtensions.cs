7f81f31306c04186db539a1adda94731a12c1f18
Dan <danjm.dev@gmail.com>
2024-11-01 14:46:29 +1300

    Added rect extensions

---
+++ Runtime/Extensions/RectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace DJM.Utilities
+{
+    public static class RectExtensions
+    {
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool Contains(this Rect rect, Rect other)
+        {
+            return rect.Contains(other.min) && rect.Contains(other.max);
+        }
+    }
+}
