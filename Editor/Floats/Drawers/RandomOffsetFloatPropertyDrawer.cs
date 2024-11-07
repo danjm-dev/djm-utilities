3ffc6116a2f95b57464237277494f4c07c3f92d1
Dan <danjm.dev@gmail.com>
2024-11-07 18:28:00 +1300

    Implemented RandomOffsetFloat and property drawer for it

---
+++ Editor/Floats/Drawers/RandomOffsetFloatPropertyDrawer.cs
@@ -0,0 +1,46 @@
+﻿using DJM.Utilities.Floats;
+using UnityEditor;
+using UnityEngine;
+
+namespace DJM.Utilities.Editor.Floats
+{
+    [CustomPropertyDrawer(typeof(RandomOffsetFloat))]
+    public class RandomOffsetFloatPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            const float maxFieldWidth = 150f;
+            const float maxPlusMinusWidth = 25f;
+            
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            
+            var baseValueWidth = position.width * 0.475f;
+            var plusMinusSymbolWidth = position.width * 0.05f;
+            var maxOffsetWidth = position.width * 0.475f;
+            
+            baseValueWidth = Mathf.Min(baseValueWidth, maxFieldWidth);
+            plusMinusSymbolWidth = Mathf.Min(plusMinusSymbolWidth, maxPlusMinusWidth);
+            maxOffsetWidth = Mathf.Min(maxOffsetWidth, maxFieldWidth);
+            
+            var baseValueRect = new Rect(position.x, position.y, baseValueWidth, position.height);
+            var plusMinusSymbolRect = new Rect(baseValueRect.xMax, position.y, plusMinusSymbolWidth, position.height);
+            var maxOffsetRect = new Rect(plusMinusSymbolRect.xMax, position.y, maxOffsetWidth, position.height);
+            
+            
+            var baseValueProperty = property.FindPropertyRelative("baseValue");
+            var maxOffsetProperty = property.FindPropertyRelative("maxOffset");
+            
+            EditorGUI.PropertyField(baseValueRect, baseValueProperty, GUIContent.none);
+            
+            var plusMinusStyle = new GUIStyle(EditorStyles.largeLabel)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 22
+            };
+            
+            EditorGUI.LabelField(plusMinusSymbolRect, "±", plusMinusStyle);
+            EditorGUI.PropertyField(maxOffsetRect, maxOffsetProperty, GUIContent.none);
+        }
+
+    }
+}
---
+++ Runtime/Floats/RandomOffsetFloat.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace DJM.Utilities.Floats
+{
+    [Serializable]
+    public struct RandomOffsetFloat : IFormattable, IEquatable<RandomOffsetFloat>
+    {
+        [SerializeField] private float baseValue;
+        [SerializeField, Min(0f)] private float maxOffset;
+        
+        public float BaseValue
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => baseValue;
+        }
+
+        public float MaxOffset
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => maxOffset;
+        }
+
+        public float Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => baseValue + UnityEngine.Random.Range(-maxOffset, maxOffset);
+        }
+
+        public float MinValue
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => baseValue - maxOffset;
+        }
+
+        public float MaxValue
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => baseValue + maxOffset;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public RandomOffsetFloat(float baseValue, float maxOffset = 0f)
+        {
+            this.baseValue = baseValue;
+            this.maxOffset = math.max(0f, maxOffset);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return $"{MinValue.ToString(format, formatProvider)} - {MaxValue.ToString(format, formatProvider)}";
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(RandomOffsetFloat other)
+        {
+            return baseValue.Equals(other.baseValue) && maxOffset.Equals(other.maxOffset);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] 
+        public override string ToString() => $"{MinValue} - {MaxValue}";
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override bool Equals(object obj) => obj is RandomOffsetFloat other && Equals(other);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() => (int)math.hash(new float2(baseValue, maxOffset));
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator float(RandomOffsetFloat randomOffsetFloat) => randomOffsetFloat.Value;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator RandomOffsetFloat(float baseValue) => new(baseValue);
+    }
+}
