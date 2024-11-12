24be48fd496431dcd7d112d49fc0166af11da6ba
Dan <danjm.dev@gmail.com>
2024-11-13 10:06:50 +1300

    CustomGizmos: moved internal rect utils into own class

--- Runtime/CustomGizmos/DrawRectCustomGizmos.cs
+++ Runtime/CustomGizmos/DrawRectCustomGizmos.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using UnityEngine;
 
 namespace DJM.Utilities.CustomGizmos
@@ -16,7 +15,9 @@ namespace DJM.Utilities.CustomGizmos
         )
         {
 #if UNITY_EDITOR
-            DrawRectInternal(center, normal, size, color);
+            SetColor(color);
+            RectCustomGizmoUtils.DrawRect(center, normal, size);
+            RevertColor();
 #endif
         }
         
@@ -31,13 +32,14 @@ namespace DJM.Utilities.CustomGizmos
         )
         {
 #if UNITY_EDITOR
-            DrawRectInternal
+            SetColor(color);
+            RectCustomGizmoUtils.DrawRect
             (
                 Get3DPosition(center, plane, positionDepth), 
                 Get2DPlaneNormal(plane), 
-                size, 
-                color
+                size
             );
+            RevertColor();
 #endif
         }
         
@@ -51,7 +53,9 @@ namespace DJM.Utilities.CustomGizmos
         )
         {
 #if UNITY_EDITOR
-            DrawRectInternal(center, Get2DPlaneNormal(plane), size, color);
+            SetColor(color);
+            RectCustomGizmoUtils.DrawRect(center, Get2DPlaneNormal(plane), size);
+            RevertColor();
 #endif
         }
         
@@ -65,7 +69,9 @@ namespace DJM.Utilities.CustomGizmos
         )
         {
 #if UNITY_EDITOR
-            DrawRectOutlineInternal(center, normal, size, color);
+            SetColor(color);
+            RectCustomGizmoUtils.DrawRectOutline(center, normal, size);
+            RevertColor();
 #endif
         }
         
@@ -80,13 +86,14 @@ namespace DJM.Utilities.CustomGizmos
         )
         {
 #if UNITY_EDITOR
-            DrawRectOutlineInternal
+            SetColor(color);
+            RectCustomGizmoUtils.DrawRectOutline
             (
                 Get3DPosition(center, plane, positionDepth), 
                 Get2DPlaneNormal(plane), 
-                size, 
-                color
+                size
             );
+            RevertColor();
 #endif
         }
         
@@ -100,45 +107,10 @@ namespace DJM.Utilities.CustomGizmos
         )
         {
 #if UNITY_EDITOR
-            DrawRectOutlineInternal(center, Get2DPlaneNormal(plane), size, color);
+            SetColor(color);
+            RectCustomGizmoUtils.DrawRectOutline(center, Get2DPlaneNormal(plane), size);
+            RevertColor();
 #endif
         }
-        
-#if UNITY_EDITOR
-        private static void DrawRectOutlineInternal(Vector3 center, Vector3 normal, Vector2 size, UnityEngine.Color? color)
-        {
-            if(size == Vector2.zero) return;
-            if(normal == Vector3.zero) return;
-            
-            SetColor(color);
-            
-            normal = normal.normalized;
-            
-            var referenceVector = Mathf.Abs(Vector3.Dot(normal, Vector3.up)) < 0.99f ? Vector3.up : Vector3.forward;
-            var xDirection = Vector3.Cross(normal, referenceVector).normalized;
-            var yDirection = Vector3.Cross(xDirection, normal).normalized;
-            
-            var xOffset = xDirection * size.x;
-            var yOffset = yDirection * size.y;
-            
-            var origin = center - xOffset * 0.5f - yOffset * 0.5f;
-            
-            PointBuffer[0] = origin;
-            PointBuffer[1] = origin + yOffset;
-            PointBuffer[2] = origin + xOffset + yOffset;
-            PointBuffer[3] = origin + xOffset;
-            
-            Gizmos.DrawLineStrip(new ReadOnlySpan<Vector3>(PointBuffer, 0, 4), true);
-
-            RevertColor();
-        }
-        
-        private static void DrawRectInternal(Vector3 center, Vector3 normal, Vector2 size, UnityEngine.Color? color)
-        {
-            SetColor(color);
-            Gizmos.DrawMesh(_rectMesh, center, Quaternion.LookRotation(-normal), size.XYO());
-            RevertColor();
-        }
-#endif
     }
 }
---
+++ Runtime/CustomGizmos/RectCustomGizmoUtils.cs
@@ -0,0 +1,51 @@
+﻿#if UNITY_EDITOR
+using System;
+using DJM.Utilities.MeshGeneration;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+
+namespace DJM.Utilities.CustomGizmos
+{
+    [InitializeOnLoad]
+    internal static class RectCustomGizmoUtils
+    {
+        private static readonly Mesh RectMesh;
+        private static readonly Vector3[] PointBuffer = new Vector3[4];
+        
+        static RectCustomGizmoUtils()
+        {
+            RectMesh = PrimitiveMeshUtils.GenerateRectMesh(1f, math.right(), math.up());
+        }
+        
+        public static void DrawRect(Vector3 center, Vector3 normal, Vector2 size)
+        {
+            Gizmos.DrawMesh(RectMesh, center, Quaternion.LookRotation(-normal), size.XYO());
+        }
+        
+        public static void DrawRectOutline(Vector3 center, Vector3 normal, Vector2 size)
+        {
+            if(size == Vector2.zero) return;
+            if(normal == Vector3.zero) return;
+            
+            normal = normal.normalized;
+            
+            var referenceVector = Mathf.Abs(Vector3.Dot(normal, Vector3.up)) < 0.99f ? Vector3.up : Vector3.forward;
+            var xDirection = Vector3.Cross(normal, referenceVector).normalized;
+            var yDirection = Vector3.Cross(xDirection, normal).normalized;
+            
+            var xOffset = xDirection * size.x;
+            var yOffset = yDirection * size.y;
+            
+            var origin = center - xOffset * 0.5f - yOffset * 0.5f;
+            
+            PointBuffer[0] = origin;
+            PointBuffer[1] = origin + yOffset;
+            PointBuffer[2] = origin + xOffset + yOffset;
+            PointBuffer[3] = origin + xOffset;
+            
+            Gizmos.DrawLineStrip(new ReadOnlySpan<Vector3>(PointBuffer), true);
+        }
+    }
+}
+#endif
