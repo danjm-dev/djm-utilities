89f77f97201aae4641f6992d8bd6796eb792c4e8
Dan <danjm.dev@gmail.com>
2024-11-25 17:43:52 +1300

    Added extensions for compute shaders

---
+++ Runtime/Extensions/Shaders/ComputeShaderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace DJM.Utilities
+{
+    public static class ComputeShaderExtensions
+    {
+        public static void Dispatch(this ComputeShader computeShader, int kernelIndex, int3 threadGroups)
+        {
+            computeShader.Dispatch(kernelIndex, threadGroups.x, threadGroups.y, threadGroups.z);
+        }
+        
+        public static void Dispatch(this ComputeShader computeShader, int kernelIndex, int2 threadGroups, int threadGroupsZ = 1)
+        {
+            computeShader.Dispatch(kernelIndex, threadGroups.x, threadGroups.y, threadGroupsZ);
+        }
+
+        public static bool TryGetKernel(this ComputeShader computeShader, string name, out int kernelIndex)
+        {
+            if (!computeShader.HasKernel(name))
+            {
+                kernelIndex = -1;
+                return false;
+            }
+            
+            kernelIndex = computeShader.FindKernel(name);
+            return true;
+        }
+    }
+}
