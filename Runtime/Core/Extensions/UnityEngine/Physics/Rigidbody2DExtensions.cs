47dc98d10d3a92aa5771347846df75943eb21bb8
Dan <danjm.dev@gmail.com>
2025-02-13 17:09:10 +1300

    Added extensions for rigidbodies

---
+++ Runtime/Core/Extensions/UnityEngine/Physics/Rigidbody2DExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DJM.Utilities
+{
+    public static class Rigidbody2DExtensions
+    {
+        public static void ClearVelocity(this Rigidbody2D rigidbody, Vector3 force)
+        {
+            rigidbody.linearVelocity = Vector2.zero;
+            rigidbody.angularVelocity = 0f;
+        }
+    }
+}
---
+++ Runtime/Core/Extensions/UnityEngine/Physics/RigidbodyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DJM.Utilities
+{
+    public static class RigidbodyExtensions
+    {
+        public static void ClearVelocity(this Rigidbody rigidbody, Vector3 force)
+        {
+            rigidbody.linearVelocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+}
