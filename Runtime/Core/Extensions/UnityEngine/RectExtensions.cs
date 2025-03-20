e336bf9411140661894466fb9b1357dc009d527a
Dan <danjm.dev@gmail.com>
2025-03-20 16:37:09 +1300

    Added padding extension for rect

--- Runtime/Core/Extensions/UnityEngine/RectExtensions.cs
+++ Runtime/Core/Extensions/UnityEngine/RectExtensions.cs
@@ -11,5 +11,20 @@ namespace DJM.Utilities
         {
             return rect.Contains(other.min) && rect.Contains(other.max);
         }
+
+        public static Rect WithPadding(this Rect rect, float padding)
+        {
+            var clampedPaddingX = Mathf.Clamp(padding, 0f, rect.width * 0.5f);
+            var clampedPaddingY = Mathf.Clamp(padding, 0f, rect.height * 0.5f);
+            
+            return new Rect
+            (
+                rect.x + clampedPaddingX,
+                rect.y + clampedPaddingY,
+                rect.width - 2 * clampedPaddingX,
+                rect.height - 2 * clampedPaddingY
+            );
+        }
+        
     }
 }
